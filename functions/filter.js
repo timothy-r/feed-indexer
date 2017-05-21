07b9fe986f0386010cb62234ebd6a1cac253f7cc
Tim Rodger <tim.rodger@gmail.com>
2017-05-21 16:17:36 +0100

    First commit

---
+++ functions/filter.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * Returns filtered results from index
+ *
+ * @param event
+ * @param context
+ * @param callback
+ */
+module.exports.handler = (event, context, callback) => {
+
+    console.log(event);
+    callback();
+
+};
---
+++ functions/indexer.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * Call indexers per feed url to update the feed index
+ *
+ * @param event
+ * @param context
+ * @param callback
+ */
+module.exports.handler = (event, context, callback) => {
+
+    console.log(event);
+    callback();
+};
